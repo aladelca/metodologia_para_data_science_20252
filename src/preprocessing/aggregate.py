2ad00b6aa8338380e228163eceb29bb7d2f42f2f
Aldo Pizarro <apizarroe13@gmail.com>
2025-09-18 14:24:43 -0500

    Actualizacion de logica para archivos AFP: Renombramiento de archivos

--- src/preprocessing/aggregate.py
+++ src/preprocessing/aggregate.py
@@ -1,16 +1,24 @@
-import pandas as pd
-
-def calculate_monthly_returns(df_raw):
+def calculate_monthly_returns(df):
     """
-    Calcula los retornos mensuales compuestos a partir de rentabilidades anuales.
+    Calcula los retornos mensuales compuestos a partir de datos de precios diarios.
 
-    Parameters:
-    annual_returns_df (pd.DataFrame): DataFrame con rentabilidades anuales de AFP
+    Parámetros:
+    df (pd.DataFrame): DataFrame con un índice de tipo DatetimeIndex y una
+                       columna de 'Close' prices.
 
-    Returns:
-    pd.DataFrame: DataFrame con los retornos mensuales compuestos
+    Retorna:
+    pd.DataFrame: DataFrame con los retornos mensuales.
     """
-    # Calcular rentabilidad mensual: (1 + anual)^(1/12) - 1
-    monthly_returns_df = (1 + df_raw) ** (1/12) - 1
+    # 1. Calcular el retorno diario como un porcentaje
+    df['Daily_Return'] = df['Close'].pct_change()
+
+    # 2. Calcular los retornos mensuales compuestos
+    # Se suma 1 para obtener el factor de crecimiento diario
+    # Se agrupa por mes ('M') y se multiplican los factores de crecimiento con .prod()
+    # Finalmente, se resta 1 para obtener el retorno porcentual del mes
+    monthly_returns = (1 + df['Daily_Return']).resample('M').prod() - 1
+
+    # Convertir a DataFrame y renombrar la columna para mayor claridad
+    monthly_returns_df = monthly_returns.to_frame(name='Monthly_Return')
     
     return monthly_returns_df
--- src/preprocessing/pipeline.py
+++ src/preprocessing/pipeline.py
@@ -1,12 +1,12 @@
-from extract import extract_all_afp_data
+from extract import extract_stock_data
 from aggregate import calculate_monthly_returns
 
-def generate_raw_data():
-    data = extract_all_afp_data()
-    data.to_parquet("data/raw/raw_pipe_afp_data.parquet")
+def generate_raw_data(ticker,period):
+    data = extract_stock_data(ticker=ticker, period=period)
+    data.to_parquet("data/raw/raw_stock_data.parquet")
     monthly_data = calculate_monthly_returns(data)
-    monthly_data.to_parquet("data/aggregated/agg_pipe_afp_data.parquet")
+    monthly_data.to_parquet("data/aggregated/agg_stock_data.parquet")
     return "success"
 
-generate_raw_data()
+generate_raw_data("SPY","max")
     
